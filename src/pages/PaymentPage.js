2437db3422cebbf13e05554f1968afb7baaf5fa7
Idosegev23 <130090986+Idosegev23@users.noreply.github.com>
2024-06-18 21:26:24 +0300

    Update PaymentPage.js

--- src/pages/PaymentPage.js
+++ src/pages/PaymentPage.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
-import axios from 'axios';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import { supabase } from '../supabaseClient';
+import styled, { createGlobalStyle } from 'styled-components';
+import axios from 'axios';  // הייבוא הנכון ש
 
 const PageContainer = styled.div`
   padding: 2rem;
