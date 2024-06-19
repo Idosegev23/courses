c3fffd1968f283482c576856303884e982945cae
Idosegev23 <130090986+Idosegev23@users.noreply.github.com>
2024-06-19 14:20:15 +0300

    Update PaymentPage.js

--- src/pages/PaymentPage.js
+++ src/pages/PaymentPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { supabase } from '../supabaseClient';
 import axios from 'axios';  // הייבוא הנכון של axios
 
 const PageContainer = styled.div`
