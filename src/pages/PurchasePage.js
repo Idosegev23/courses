8847c45f1fa18d52659c233ae40d51f722df5468
Idosegev23 <130090986+Idosegev23@users.noreply.github.com>
2024-06-30 16:53:06 +0300

    Initial commit

--- src/pages/PurchasePage.js
+++ src/pages/PurchasePage.js
@@ -158,9 +158,9 @@ const PurchasePage = () => {
 
   const getJwtToken = async () => {
     try {
-      const response = await axios.post('https://cors-anywhere.herokuapp.com/https://sandbox.d.greeninvoice.co.il/api/v1/account/token', {
-        id: 'd8281ab1-2ebc-44a9-a53f-e19a46b879dc',
-        secret: 'f5gxE9n2H43sY4d-P-Ivhg'
+      const response = await axios.post('https://sandbox.d.greeninvoice.co.il/api/v1/account/token', {
+        id: process.env.REACT_APP_API_KEY_GREEN_INVOICE_TEST,
+        secret: process.env.REACT_APP_API_SECRET_GREEN_INVOICE_TEST
       }, {
         headers: {
           'Content-Type': 'application/json'
@@ -215,7 +215,7 @@ const PurchasePage = () => {
     };
 
     try {
-      const response = await axios.post('https://cors-anywhere.herokuapp.com/https://sandbox.d.greeninvoice.co.il/api/v1/payments/form', invoiceData, {
+      const response = await axios.post('https://sandbox.d.greeninvoice.co.il/api/v1/payments/form', invoiceData, {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${token}`
