e5240b654413c6ffd3b4122047542d2deca8e9bb
Idosegev23 <130090986+Idosegev23@users.noreply.github.com>
2024-07-01 11:30:41 +0300

    Updated project with latest changes for payment flow and popup handling

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-// api/proxy.js
+// pages/api/proxy.js
 
 const axios = require('axios');
 
@@ -6,22 +6,19 @@ const API_BASE_URL = 'https://sandbox.d.greeninvoice.co.il/api/v1';
 
 module.exports = async (req, res) => {
   if (req.method === 'POST') {
-    const { id, secret } = req.body;
+    const { endpoint, data, token } = req.body;
 
-    if (id && secret) {
-      try {
-        const response = await axios.post(`${API_BASE_URL}/account/token`, {
-          id,
-          secret
-        });
-        const token = response.data.token;
-        res.status(200).json({ token });
-      } catch (error) {
-        console.error('Error requesting token:', error);
-        res.status(500).json({ error: 'Failed to obtain token' });
-      }
-    } else {
-      res.status(400).json({ error: "Invalid ID or secret" });
+    try {
+      const response = await axios.post(`${API_BASE_URL}/${endpoint}`, data, {
+        headers: {
+          'Content-Type': 'application/json',
+          ...(token && { Authorization: `Bearer ${token}` })
+        }
+      });
+      res.status(200).json(response.data);
+    } catch (error) {
+      console.error('Error requesting API:', error);
+      res.status(500).json({ error: 'Failed to request API' });
     }
   } else {
     res.status(405).json({ error: "Method not allowed" });
