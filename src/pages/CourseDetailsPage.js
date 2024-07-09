7ccec521cc769353b2628a9132752617a57de2de
Idosegev23 <130090986+Idosegev23@users.noreply.github.com>
2024-07-09 12:17:46 +0300

    הוספת פונקציונליות לניהול משתמשים במערכת הניהול

--- src/pages/CourseDetailsPage.js
+++ src/pages/CourseDetailsPage.js
@@ -537,7 +537,7 @@ const CourseDetailsPage = () => {
           address: userData.street_address,
           city: userData.city,
           country: "IL",
-          phone: userData.phone_num.startsWith('0') ? userData.phone_num : `0${userData.phone_num}`,
+          phone: `0${userData.phone_num}`,
           add: true
         },
         successUrl: `${process.env.REACT_APP_API_URL}/personal-area?status=success`,
